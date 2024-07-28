8505909056204bc1d0063c484347228971614119
chandu1112 <98867418+chandu1112@users.noreply.github.com>
2024-07-28 15:29:27 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -16,16 +16,16 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 'TOKEN'
+TOKEN = '7255621314:AAFYTrsoXuQqyO6zXRYAwjynPBFEQKKMJYw'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002224570220
-CHANNEL_ID = -1002224570220
-error_channel_id = -1002224570220
+FORWARD_CHANNEL_ID = -1002168978553
+CHANNEL_ID = -1002168978553
+error_channel_id = -1002168978553
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
-db = client['VENOM']
+db = client['BLACK']
 users_collection = db.users
 
 bot = telebot.TeleBot(TOKEN)
